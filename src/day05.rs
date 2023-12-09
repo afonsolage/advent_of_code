ded5d088d1b77c90755fe947fe38bb180bb85f72
Afonso Lage <lage.afonso@gmail.com>
2023-12-09 03:49:27 -0300

    Finished day 05 part 02

--- src/day05.rs
+++ src/day05.rs
@@ -1,12 +1,50 @@
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 struct Range {
-    destination: u32,
-    source: u32,
-    length: u32,
+    start: u64,
+    lenght: u64,
 }
 
 impl Range {
-    fn map(&self, target: u32) -> Option<u32> {
+    fn iter(&self) -> impl Iterator<Item = u64> {
+        self.start..(self.start + self.lenght).into()
+    }
+
+    fn end(&self) -> u64 {
+        self.start + self.lenght - 1
+    }
+}
+
+impl From<&[u64]> for Range {
+    fn from(value: &[u64]) -> Self {
+        Range {
+            start: value[0],
+            lenght: value[1],
+        }
+    }
+}
+
+impl From<u64> for Range {
+    fn from(value: u64) -> Self {
+        Range {
+            start: value,
+            lenght: 1,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+struct MapRange {
+    destination: u64,
+    source: u64,
+    length: u64,
+}
+
+impl MapRange {
+    fn source_end(&self) -> u64 {
+        self.source + self.length - 1
+    }
+
+    fn map_single(&self, target: u64) -> Option<u64> {
         if target >= self.source && (target - self.source) < self.length {
             let offset = target - self.source;
             Some(self.destination + offset)
@@ -14,13 +52,57 @@ impl Range {
             None
         }
     }
+
+    fn map_range(&self, target: Range) -> (Option<Range>, Vec<Range>) {
+        if let Some((inside, outside)) = self.split(target) {
+            let offset = inside.start - self.source;
+            let mapped = Range {
+                start: self.destination + offset,
+                ..inside
+            };
+            (Some(mapped), outside)
+        } else {
+            (None, vec![target])
+        }
+    }
+
+    fn split(&self, target: Range) -> Option<(Range, Vec<Range>)> {
+        if target.end() < self.source || target.start > self.source_end() {
+            return None;
+        }
+
+        let mut outside = vec![];
+        let left_split = target.start.max(self.source);
+        let right_split = target.end().min(self.source_end());
+
+        let inside = Range {
+            start: left_split,
+            lenght: right_split - left_split + 1,
+        };
+
+        if left_split > target.start {
+            outside.push(Range {
+                start: target.start,
+                lenght: left_split - target.start,
+            })
+        };
+
+        if right_split < target.end() {
+            outside.push(Range {
+                start: right_split + 1,
+                lenght: target.end() - right_split,
+            })
+        }
+
+        Some((inside, outside))
+    }
 }
 
-impl From<&str> for Range {
+impl From<&str> for MapRange {
     fn from(value: &str) -> Self {
         let values = value
             .split_whitespace()
-            .filter_map(|c| c.parse::<u32>().ok())
+            .filter_map(|c| c.parse::<u64>().ok())
             .collect::<Vec<_>>();
         Self {
             destination: values[0],
@@ -32,40 +114,86 @@ impl From<&str> for Range {
 
 #[derive(Debug, Clone, Default)]
 struct Alamanac {
-    seeds: Vec<u32>,
-    seed_to_soil: Vec<Range>,
-    soil_to_fertilizer: Vec<Range>,
-    fertilizer_to_water: Vec<Range>,
-    water_to_light: Vec<Range>,
-    light_to_temperature: Vec<Range>,
-    temperature_to_humidity: Vec<Range>,
-    humidity_to_location: Vec<Range>,
+    seeds: Vec<Range>,
+    seed_to_soil: Vec<MapRange>,
+    soil_to_fertilizer: Vec<MapRange>,
+    fertilizer_to_water: Vec<MapRange>,
+    water_to_light: Vec<MapRange>,
+    light_to_temperature: Vec<MapRange>,
+    temperature_to_humidity: Vec<MapRange>,
+    humidity_to_location: Vec<MapRange>,
 }
 
 impl Alamanac {
-    fn new(seeds: Vec<u32>) -> Self {
+    fn new(seeds: Vec<Range>) -> Self {
         Self {
             seeds,
             ..Default::default()
         }
     }
 
-    fn find_lowest_location(&self) -> u32 {
+    fn find_lowest_location(&self) -> u64 {
         self.seeds
             .iter()
-            .map(|&seed| self.get_seed_location(seed))
+            .flat_map(|seed| seed.iter())
+            .into_iter()
+            .map(|seed| self.get_seed_location(seed))
             .min()
             .unwrap()
     }
 
-    fn find_mapping(&self, target: u32, ranges: &[Range]) -> u32 {
+    fn find_lowest_ranged_location(&self) -> u64 {
+        self.seeds
+            .iter()
+            .map(|&seed| self.get_seed_range_location(seed))
+            .flat_map(|ranges| ranges.into_iter().map(|range| range.start))
+            .min()
+            .unwrap()
+    }
+
+    fn find_range_mapping(&self, targets: &[Range], ranges: &[MapRange]) -> Vec<Range> {
+        let (mapped_targets, unmapped) = ranges.iter().fold(
+            (vec![], targets.to_vec()),
+            |(mut mapped_targets, unmapped_targets), &map_range| {
+                let (mapped, unmapped): (Vec<_>, Vec<_>) = unmapped_targets
+                    .iter()
+                    .map(|&target| map_range.map_range(target))
+                    .unzip();
+
+                let mut mapped = mapped.into_iter().filter_map(|r| r).collect::<Vec<_>>();
+                let unmapped_targets = unmapped.into_iter().flatten().collect::<Vec<_>>();
+
+                mapped_targets.append(&mut mapped);
+                (mapped_targets, unmapped_targets)
+            },
+        );
+
+        mapped_targets
+            .into_iter()
+            .chain(unmapped.into_iter())
+            .collect()
+    }
+
+    fn find_mapping(&self, target: u64, ranges: &[MapRange]) -> u64 {
         ranges
             .iter()
-            .find_map(|range| range.map(target))
+            .find_map(|range| range.map_single(target))
             .unwrap_or(target)
     }
 
-    fn get_seed_location(&self, seed: u32) -> u32 {
+    fn get_seed_range_location(&self, seed: Range) -> Vec<Range> {
+        let soil = self.find_range_mapping(&vec![seed], &self.seed_to_soil);
+        let fertilizer = self.find_range_mapping(&soil, &self.soil_to_fertilizer);
+        let water = self.find_range_mapping(&fertilizer, &self.fertilizer_to_water);
+        let light = self.find_range_mapping(&water, &self.water_to_light);
+        let temperature = self.find_range_mapping(&light, &self.light_to_temperature);
+        let humidity = self.find_range_mapping(&temperature, &self.temperature_to_humidity);
+        let location = self.find_range_mapping(&humidity, &self.humidity_to_location);
+
+        location
+    }
+
+    fn get_seed_location(&self, seed: u64) -> u64 {
         let soil = self.find_mapping(seed, &self.seed_to_soil);
         let fertilizer = self.find_mapping(soil, &self.soil_to_fertilizer);
         let water = self.find_mapping(fertilizer, &self.fertilizer_to_water);
@@ -78,7 +206,7 @@ impl Alamanac {
     }
 }
 
-fn part02(input: &str) -> u32 {
+fn part02(input: &str) -> u64 {
     let mut split = input.split("\n\n");
 
     let values = split
@@ -86,17 +214,14 @@ fn part02(input: &str) -> u32 {
         .unwrap()
         .trim_start_matches("seed: ")
         .split_whitespace()
-        .filter_map(|seed| seed.parse::<u32>().ok())
+        .filter_map(|seed| seed.parse::<u64>().ok())
         .collect::<Vec<_>>();
 
     let seeds = values
         .chunks(2)
-        .flat_map(|pair| {
-            let begin = pair[0];
-            let len = pair[1];
-            begin..(begin + len)
-        })
-        .collect();
+        .map(|pair| pair.into())
+        .collect::<Vec<Range>>();
+
     let almanac = Alamanac::new(seeds);
 
     let almanac = split.fold(almanac, |mut almanac, raw| {
@@ -106,7 +231,7 @@ fn part02(input: &str) -> u32 {
             .lines()
             .filter_map(|raw| {
                 if !raw.is_empty() {
-                    Some::<Range>(raw.into())
+                    Some::<MapRange>(raw.into())
                 } else {
                     None
                 }
@@ -127,17 +252,17 @@ fn part02(input: &str) -> u32 {
         almanac
     });
 
-    almanac.find_lowest_location()
+    almanac.find_lowest_ranged_location()
 }
 
-fn part01(input: &str) -> u32 {
+fn part01(input: &str) -> u64 {
     let mut split = input.split("\n\n");
     let seeds = split
         .next()
         .unwrap()
         .trim_start_matches("seed: ")
         .split_whitespace()
-        .filter_map(|seed| seed.parse::<u32>().ok())
+        .filter_map(|seed| seed.parse::<u64>().ok().and_then(|s| Some(s.into())))
         .collect();
 
     let almanac = Alamanac::new(seeds);
@@ -149,7 +274,7 @@ fn part01(input: &str) -> u32 {
             .lines()
             .filter_map(|raw| {
                 if !raw.is_empty() {
-                    Some::<Range>(raw.into())
+                    Some::<MapRange>(raw.into())
                 } else {
                     None
                 }
@@ -181,6 +306,152 @@ fn main() {
 
 #[cfg(test)]
 mod test {
+    use super::*;
+
+    #[test]
+    fn split_outside() {
+        let map_range = MapRange {
+            destination: 30,
+            source: 8,
+            length: 7,
+        };
+
+        let after_range = Range {
+            start: 15,
+            lenght: 3,
+        };
+        let before_range = Range {
+            start: 5,
+            lenght: 3,
+        };
+        assert_eq!(map_range.split(after_range), None);
+        assert_eq!(map_range.split(before_range), None);
+    }
+
+    #[test]
+    fn split_part_inside() {
+        let map_range = MapRange {
+            destination: 30,
+            source: 8,
+            length: 7,
+        };
+        let part_inside_range = Range {
+            start: 13,
+            lenght: 7,
+        };
+        assert_eq!(
+            map_range.split(part_inside_range),
+            Some((
+                Range {
+                    start: 13,
+                    lenght: 2,
+                },
+                vec![Range {
+                    start: 15,
+                    lenght: 5,
+                },]
+            ))
+        );
+    }
+
+    #[test]
+    fn split_full_inside() {
+        let map_range = MapRange {
+            destination: 30,
+            source: 8,
+            length: 7,
+        };
+
+        let full_inside_range = Range {
+            start: 10,
+            lenght: 3,
+        };
+
+        assert_eq!(
+            map_range.split(full_inside_range),
+            Some((
+                Range {
+                    start: 10,
+                    lenght: 3,
+                },
+                vec![],
+            ))
+        );
+    }
+
+    #[test]
+    fn map_range_outside() {
+        let map_range = MapRange {
+            destination: 30,
+            source: 8,
+            length: 7,
+        };
+
+        let after_range = Range {
+            start: 15,
+            lenght: 3,
+        };
+        let before_range = Range {
+            start: 5,
+            lenght: 3,
+        };
+        assert_eq!(map_range.map_range(after_range), (None, vec![after_range]));
+        assert_eq!(
+            map_range.map_range(before_range),
+            (None, vec![before_range])
+        );
+    }
+
+    #[test]
+    fn map_range_part_inside() {
+        let map_range = MapRange {
+            destination: 30,
+            source: 8,
+            length: 7,
+        };
+        let part_inside_range = Range {
+            start: 13,
+            lenght: 7,
+        };
+        assert_eq!(
+            map_range.map_range(part_inside_range),
+            (
+                Some(Range {
+                    start: 35,
+                    lenght: 2,
+                }),
+                vec![Range {
+                    start: 15,
+                    lenght: 5,
+                },]
+            )
+        );
+    }
+
+    #[test]
+    fn map_range_full_inside() {
+        let map_range = MapRange {
+            destination: 30,
+            source: 8,
+            length: 7,
+        };
+
+        let full_inside_range = Range {
+            start: 10,
+            lenght: 3,
+        };
+
+        assert_eq!(
+            map_range.map_range(full_inside_range),
+            (
+                Some(Range {
+                    start: 32,
+                    lenght: 3,
+                }),
+                vec![],
+            )
+        );
+    }
 
     #[test]
     fn part01() {
