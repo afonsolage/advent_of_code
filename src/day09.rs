05c8e9c1b992f98fa60e924e07e4899f674acb5d
Afonso Lage <lage.afonso@gmail.com>
2023-12-10 10:26:31 -0300

    Finished day 09 part 02

--- src/day09.rs
+++ src/day09.rs
@@ -31,12 +31,45 @@ fn predict(line: &str) -> i64 {
     diff + values.last().unwrap()
 }
 
+fn predict_back(line: &str) -> i64 {
+    let values = line
+        .split_whitespace()
+        .map(|c| c.parse::<i64>().unwrap())
+        .collect::<Vec<_>>();
+
+    let mut interpolation = values.windows(2).map(|w| w[1] - w[0]).collect::<Vec<_>>();
+    let mut interpolations = vec![interpolation];
+
+    loop {
+        interpolation = interpolations
+            .last()
+            .unwrap()
+            .windows(2)
+            .map(|w| w[0] - w[1])
+            .collect::<Vec<_>>();
+
+        if interpolation.iter().all(|&v| v == 0) {
+            break;
+        }
+
+        interpolations.push(interpolation);
+    }
+
+    let diff = interpolations
+        .into_iter()
+        .rev()
+        .map(|interpolation| *interpolation.first().unwrap())
+        .sum::<i64>();
+
+    values.first().unwrap() - diff
+}
+
 fn part01(input: &str) -> i64 {
     input.lines().map(|line| predict(line)).sum()
 }
 
-fn part02(_input: &str) -> i64 {
-    0
+fn part02(input: &str) -> i64 {
+    input.lines().map(|line| predict_back(line)).sum()
 }
 
 fn main() {
@@ -64,6 +97,21 @@ mod test {
     }
 
     #[test]
+    fn predict_back_line_1() {
+        assert_eq!(super::predict_back("0 3 6 9 12 15"), -3);
+    }
+
+    #[test]
+    fn predict_back_line_2() {
+        assert_eq!(super::predict_back("1 3 6 10 15 21"), 0);
+    }
+
+    #[test]
+    fn predict_back_line_3() {
+        assert_eq!(super::predict_back("10 13 16 21 30 45"), 5);
+    }
+
+    #[test]
     fn part01() {
         let input = "0 3 6 9 12 15
 1 3 6 10 15 21
@@ -74,8 +122,10 @@ mod test {
 
     #[test]
     fn part02() {
-        let input = "";
-
-        assert_eq!(super::part02(input), 0);
+        let input = "0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45
+";
+        assert_eq!(super::part02(input), 2);
     }
 }
