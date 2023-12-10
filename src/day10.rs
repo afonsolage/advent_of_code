d245bcd4ffd7e1b12f3d848d64ccd03f0520e1da
Afonso Lage <lage.afonso@gmail.com>
2023-12-10 17:24:15 -0300

    WIP: day 10 part 02

--- src/day10.rs
+++ src/day10.rs
@@ -1,3 +1,5 @@
+use std::collections::{HashMap, HashSet};
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 enum Tile {
     Vertical,
@@ -25,7 +27,7 @@ impl std::fmt::Display for Tile {
     }
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 struct Pos {
     x: isize,
     y: isize,
@@ -189,8 +191,94 @@ fn calc_starting_connections(map: &Vec<Tile>, width: isize, start_pos: Pos) -> (
     (result[0], result[1])
 }
 
-fn part02(_input: &str) -> u64 {
-    0
+fn part02(input: &str) -> u64 {
+    let map = input
+        .lines()
+        .flat_map(|line| line.chars().map(|c| c.into()))
+        .collect::<Vec<Tile>>();
+
+    let width = (map.len() as f64).sqrt() as isize;
+    let start = map.iter().position(|&t| t == Tile::Starting).unwrap();
+    let start_pos = Pos::from_index(width, start as isize);
+
+    let next = |previous: Pos, current: Pos| -> Pos {
+        let pipe = map[current.to_index(width) as usize];
+        let [mut a, mut b] = pipe.connections();
+
+        a = a + current;
+        b = b + current;
+
+        if previous == a {
+            b
+        } else {
+            a
+        }
+    };
+
+    let (mut a, mut b) = { calc_starting_connections(&map, width, start_pos) };
+
+    let mut previous_a = start_pos;
+    let mut previous_b = start_pos;
+    let mut loop_map = HashSet::new();
+
+    loop {
+        let next_a = next(previous_a, a);
+        let next_b = next(previous_b, b);
+
+        if next_a == next_b {
+            loop_map.insert(next_a);
+            break;
+        }
+
+        previous_a = a;
+        previous_b = b;
+        a = next_a;
+        b = next_b;
+    }
+
+    let mut inside_count = 0;
+
+    for x in 0..width {
+        for y in 0..width {
+            let pos = Pos { x, y };
+
+            if loop_map.contains(&pos) {
+                continue;
+            }
+
+            if count_intersections(width, Pos { x: -1, y: 0 }, pos, &loop_map) % 2 == 0 {
+                continue;
+            }
+            if count_intersections(width, Pos { x: 1, y: 0 }, pos, &loop_map) % 2 == 0 {
+                continue;
+            }
+            if count_intersections(width, Pos { x: 0, y: 1 }, pos, &loop_map) % 2 == 0 {
+                continue;
+            }
+            if count_intersections(width, Pos { x: 0, y: -1 }, pos, &loop_map) % 2 == 0 {
+                continue;
+            }
+
+            inside_count += 1;
+        }
+    }
+
+    inside_count
+}
+
+fn count_intersections(width: isize, dir: Pos, position: Pos, map: &HashSet<Pos>) -> usize {
+    let mut intersection_count = 0;
+    let mut next_pos = position + dir;
+
+    while next_pos.x >= 0 && next_pos.y >= 0 && next_pos.x < width && next_pos.y < width {
+        if map.contains(&next_pos) {
+            intersection_count += 1;
+        }
+
+        next_pos = next_pos + dir;
+    }
+
+    intersection_count
 }
 
 fn main() {
@@ -225,8 +313,48 @@ LJ...
     }
 
     #[test]
-    fn part02() {
-        let input = "";
-        assert_eq!(super::part02(input), 0);
+    fn part02_1() {
+        let input = "...........
+.S-------7.
+.|F-----7|.
+.||.....||.
+.||.....||.
+.|L-7.F-J|.
+.|..|.|..|.
+.L--J.L--J.
+...........
+";
+        assert_eq!(super::part02(input), 4);
+    }
+
+    #[test]
+    fn part02_2() {
+        let input = ".F----7F7F7F7F-7....
+.|F--7||||||||FJ....
+.||.FJ||||||||L7....
+FJL7L7LJLJ||LJ.L-7..
+L--J.L7...LJS7F-7L7.
+....F-J..F7FJ|L7L7L7
+....L7.F7||L7|.L7L7|
+.....|FJLJ|FJ|F7|.LJ
+....FJL-7.||.||||...
+....L---J.LJ.LJLJ...";
+        assert_eq!(super::part02(input), 8);
+    }
+
+    #[test]
+    fn part02_3() {
+        let input = "FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJ7F7FJ-
+L---JF-JLJ.||-FJLJJ7
+|F|F-JF---7F7-L7L|7|
+|FFJF7L7F-JF7|JL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L
+";
+        assert_eq!(super::part02(input), 10);
     }
 }
