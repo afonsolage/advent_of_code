af88088a3b3e05c5a9ca9a17381ebc20458ad3d9
Afonso Lage <lage.afonso@gmail.com>
2023-12-28 03:43:04 -0300

    Finished day 12 part 02

--- src/day13.rs
+++ src/day13.rs
@@ -1,7 +1,7 @@
-fn count_mirroed_left_line(line: &[u64]) -> u64 {
-    (1..(line.len() - 1))
+fn count_mirroed(line: &[u64]) -> u64 {
+    (1..(line.len()))
         .map(|mid| line.split_at(mid))
-        .find_map(|(left, right)| {
+        .filter_map(|(left, right)| {
             let min_len = left.len().min(right.len());
             let last = left.len() - 1;
             if (0..min_len).all(|idx| left[last - idx] == right[idx]) {
@@ -10,6 +10,7 @@ fn count_mirroed_left_line(line: &[u64]) -> u64 {
                 None
             }
         })
+        .max()
         .unwrap_or(0)
 }
 
@@ -68,8 +69,8 @@ fn part01(input: &str) -> u64 {
         .split("\n\n")
         .map(|segment| (parse_line(segment), parse_column(segment)))
         .map(|(line, column)| {
-            let line_cnt = count_mirroed_left_line(&line);
-            let column_cnt = count_mirroed_left_line(&column);
+            let line_cnt = count_mirroed(&line);
+            let column_cnt = count_mirroed(&column);
             line_cnt + column_cnt * 100
         })
         .sum()
@@ -88,45 +89,45 @@ fn main() {
 #[cfg(test)]
 mod test {
 
-    #[test]
-    fn parse_line() {
-        assert_eq!(
-            super::parse_line(
-                "#.##..##.
-..#.##.#.
-##......#
-##......#
-..#.##.#.
-..##..##.
-#.#.##.#."
-            ),
-            vec![77, 12, 115, 33, 82, 82, 33, 115, 12]
-        );
-    }
-
-    #[test]
-    fn parse_column() {
-        assert_eq!(
-            super::parse_column(
-                "#...##..#
-#....#..#
-..##..###
-#####.##.
-#####.##.
-..##..###
-#....#..#"
-            ),
-            vec![305, 289, 460, 223, 223, 460, 289]
-        )
-    }
-
-    #[test]
-    fn count_mirroed_left_line() {
-        assert_eq!(
-            super::count_mirroed_left_line(&[77, 12, 115, 33, 82, 82, 33, 115, 12]),
-            5
-        )
-    }
+    //     #[test]
+    //     fn parse_line() {
+    //         assert_eq!(
+    //             super::parse_line(
+    //                 "#.##..##.
+    // ..#.##.#.
+    // ##......#
+    // ##......#
+    // ..#.##.#.
+    // ..##..##.
+    // #.#.##.#."
+    //             ),
+    //             vec![77, 12, 115, 33, 82, 82, 33, 115, 12]
+    //         );
+    //     }
+    //
+    //     #[test]
+    //     fn parse_column() {
+    //         assert_eq!(
+    //             super::parse_column(
+    //                 "#...##..#
+    // #....#..#
+    // ..##..###
+    // #####.##.
+    // #####.##.
+    // ..##..###
+    // #....#..#"
+    //             ),
+    //             vec![305, 289, 460, 223, 223, 460, 289]
+    //         )
+    //     }
+    //
+    //     #[test]
+    //     fn count_mirroed_left_line() {
+    //         assert_eq!(
+    //             super::count_mirroed(&[77, 12, 115, 33, 82, 82, 33, 115, 12]),
+    //             5
+    //         )
+    //     }
 
     #[test]
     fn part01() {
