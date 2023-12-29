6926daa046e09fd277216a7540ed39afb1c1706a
Afonso Lage <lage.afonso@gmail.com>
2023-12-29 18:53:04 -0300

    Finished day 15 part 02

--- src/day15.rs
+++ src/day15.rs
@@ -9,8 +9,35 @@ fn part01(input: &str) -> u64 {
     input.split(',').map(hash).sum()
 }
 
-fn part02(_input: &str) -> u64 {
-    0
+fn part02(input: &str) -> u64 {
+    let input = input.trim_matches(|c: char| c.is_ascii_whitespace());
+    let mut map: Vec<Vec<(&str, u64)>> = vec![vec![]; 256];
+
+    input.split(',').for_each(|step| {
+        if let Some(label) = step.strip_suffix('-') {
+            let hash = hash(label);
+            map[hash as usize].retain(|pair| pair.0 != label);
+        } else {
+            let (label, focal_len) = step.split_once('=').unwrap();
+            let hash = hash(label);
+            let focal_len = focal_len.parse::<u64>().unwrap();
+            if let Some(pair) = map[hash as usize].iter_mut().find(|pair| pair.0 == label) {
+                pair.1 = focal_len
+            } else {
+                map[hash as usize].push((label, focal_len));
+            }
+        }
+    });
+
+    map.into_iter()
+        .enumerate()
+        .flat_map(|(box_idx, box_content)| {
+            box_content
+                .into_iter()
+                .enumerate()
+                .map(move |(slot, pair)| (box_idx as u64 + 1) * (slot as u64 + 1) * pair.1)
+        })
+        .sum()
 }
 
 fn main() {
@@ -36,9 +63,9 @@ mod test {
 
     #[test]
     fn part02() {
-        let input = "";
+        let input = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7";
 
-        assert_eq!(super::part02(input), 0);
+        assert_eq!(super::part02(input), 145);
     }
 }
 
