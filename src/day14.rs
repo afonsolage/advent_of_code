1e0ca6e2c4cc13eaf64d91dfe395089630127b04
Afonso Lage <lage.afonso@gmail.com>
2023-12-29 17:16:22 -0300

    Finished day 14 part 02

--- src/day14.rs
+++ src/day14.rs
@@ -1,3 +1,8 @@
+use std::{
+    collections::{hash_map::DefaultHasher, HashMap, HashSet},
+    hash::{Hash, Hasher},
+};
+
 fn calc_row_load(row: &[char]) -> u64 {
     let row_count = row.len();
     row.iter()
@@ -11,21 +16,83 @@ fn calc_platform_load(platform: &[Vec<char>]) -> u64 {
     platform.iter().map(|row| calc_row_load(row)).sum()
 }
 
-fn tilt_north(platform: Vec<Vec<char>>) -> Vec<Vec<char>> {
-    platform
-        .into_iter()
-        .map(|mut column| {
-            for n in 0..column.len() {
-                if column[n] == '.' {
-                    let next = n + column.iter().skip(n).take_while(|&&c| c == '.').count();
-                    if next < column.len() && column[next] == 'O' {
-                        column.swap(n, next);
-                    }
+fn tilt_row(row: &mut [char]) {}
+
+fn tilt_north(platform: &mut [Vec<char>]) {
+    platform.iter_mut().for_each(|column| {
+        for n in 0..column.len() {
+            if column[n] == '.' {
+                let next = n + column.iter().skip(n).take_while(|&&c| c == '.').count();
+                if next < column.len() && column[next] == 'O' {
+                    column.swap(n, next);
                 }
             }
-            column
-        })
-        .collect()
+        }
+    });
+}
+
+fn tilt_west(platform: &mut [Vec<char>]) {
+    let column_count = platform.len();
+    let row_count = platform.first().unwrap().len();
+
+    for r in 0..row_count {
+        for c in 0..(column_count - 1) {
+            if platform[c][r] == '.' {
+                let mut next = c + 1;
+                while next < (column_count - 1) && platform[next][r] == '.' {
+                    next += 1;
+                }
+
+                if platform[next][r] == 'O' {
+                    let tmp = platform[c][r];
+                    platform[c][r] = platform[next][r];
+                    platform[next][r] = tmp;
+                }
+            }
+        }
+    }
+}
+
+fn tilt_south(platform: &mut [Vec<char>]) {
+    let column_count = platform.len();
+    let row_count = platform.first().unwrap().len();
+
+    platform.iter_mut().for_each(|column| {
+        for r in (1..row_count).rev() {
+            if column[r] == '.' {
+                let mut prev = r - 1;
+                while prev > 0 && column[prev] == '.' {
+                    prev -= 1;
+                }
+
+                if column[prev] == 'O' {
+                    column.swap(prev, r);
+                }
+            }
+        }
+    });
+}
+
+fn tilt_east(platform: &mut [Vec<char>]) {
+    let column_count = platform.len();
+    let row_count = platform.first().unwrap().len();
+
+    for r in 0..row_count {
+        for c in (1..column_count).rev() {
+            if platform[c][r] == '.' {
+                let mut prev = c - 1;
+                while prev > 0 && platform[prev][r] == '.' {
+                    prev -= 1;
+                }
+
+                if platform[prev][r] == 'O' {
+                    let tmp = platform[c][r];
+                    platform[c][r] = platform[prev][r];
+                    platform[prev][r] = tmp;
+                }
+            }
+        }
+    }
 }
 
 fn transpose(platform: Vec<Vec<char>>) -> Vec<Vec<char>> {
@@ -59,41 +126,74 @@ fn parse(input: &str) -> Vec<Vec<char>> {
     )
 }
 
-fn print_platform(platform: &[Vec<char>]) {
-    platform.iter().for_each(|line| {
-        line.iter().for_each(|c| print!("{}", c));
+fn print(platform: &[Vec<char>]) {
+    let row_count = platform.first().unwrap().len();
+    let column_count = platform.len();
+    for c in 0..column_count {
+        for row in platform {
+            print!("{}", row[c]);
+        }
         println!();
-    });
-
+    }
     println!();
 }
 
+fn hash(platform: &[Vec<char>]) -> u64 {
+    let mut hasher = DefaultHasher::new();
+
+    platform.hash(&mut hasher);
+
+    hasher.finish()
+}
+
 fn part01(input: &str) -> u64 {
-    let platform = parse(input);
-    let tilted_platform = tilt_north(platform);
-    calc_platform_load(&tilted_platform)
+    let mut platform = parse(input);
+    tilt_north(&mut platform);
+    calc_platform_load(&platform)
+}
+
+struct CacheablePlatform(u64, Vec<Vec<char>>);
+
+fn cycle(cache: &mut HashMap<u64, Vec<Vec<char>>>, platform: &mut CacheablePlatform) {
+    if let Some(cached) = cache.get(&platform.0) {
+        platform
+            .1
+            .iter_mut()
+            .zip(cached)
+            .for_each(|(column, cached_column)| column.copy_from_slice(cached_column));
+    } else {
+        tilt_north(&mut platform.1);
+        tilt_west(&mut platform.1);
+        tilt_south(&mut platform.1);
+        tilt_east(&mut platform.1);
+        platform.0 = hash(&platform.1);
+    }
 }
 
 fn part02(input: &str) -> u64 {
     let platform = parse(input);
-    print_platform(&platform);
-    let platform = transpose(platform);
-    print_platform(&platform);
-    let platform = transpose(platform);
-    print_platform(&platform);
-    let platform = transpose(platform);
-    print_platform(&platform);
-    64
+    let hash = hash(&platform);
+
+    let mut platform = CacheablePlatform(hash, platform);
+    let mut cache = HashMap::new();
+
+    for n in 0..1000 {
+        cycle(&mut cache, &mut platform);
+    }
+
+    calc_platform_load(&platform.1)
 }
 
 fn main() {
     let input = include_str!("../input/day14.input");
     println!("Part 01: {}", part01(input));
-    // println!("Part 02: {}", part02(input));
+    println!("Part 02: {}", part02(input));
 }
 
 #[cfg(test)]
 mod test {
+    use std::collections::HashMap;
+
     const INPUT: &str = "O....#....
 O.OO#....#
 .....##...
@@ -118,10 +218,12 @@ O..#.OO...
 
     #[test]
     fn tilt_north() {
-        let platform = super::parse(INPUT);
+        let mut platform = super::parse(INPUT);
         let tilted_platform = super::parse(TILTED_INPUT);
 
-        assert_eq!(super::tilt_north(platform), tilted_platform);
+        super::tilt_north(&mut platform);
+
+        assert_eq!(platform, tilted_platform);
     }
 
     #[test]
@@ -137,9 +239,9 @@ O..#.OO...
         assert_eq!(super::part01(INPUT), 136);
     }
 
-    // #[test]
-    // fn part02() {
-    //     assert_eq!(super::part02(INPUT), 0);
-    // }
+    #[test]
+    fn part02() {
+        assert_eq!(super::part02(INPUT), 64);
+    }
 }
 
