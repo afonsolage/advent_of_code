3cc9986be13fe34cc6859a13b9eb6be1074719a1
Afonso Lage <lage.afonso@gmail.com>
2023-12-31 17:51:54 -0300

    Finished day 17 part 02

--- src/day17.rs
+++ src/day17.rs
@@ -1,15 +1,14 @@
 use std::collections::{BinaryHeap, HashMap, HashSet};
 
 #[derive(Debug, Clone, Eq)]
-struct Node {
-    path: Vec<(i32, i32)>,
+struct Crucible {
     heat_loss: u32,
     pos: (i32, i32),
     dir: Dir,
     consecultive_dir: u32,
 }
 
-impl Node {
+impl Crucible {
     fn get_successors(&self, map: &[Vec<u32>]) -> [Option<Self>; 3] {
         let width = map[0].len();
         let height = map.len();
@@ -43,11 +42,56 @@ impl Node {
                 1
             };
 
-            let mut next_path = self.path.clone();
-            next_path.push(next_pos);
+            successors[idx] = Some(Crucible {
+                heat_loss: next_heat_loss,
+                pos: next_pos,
+                dir,
+                consecultive_dir: next_consecultive_dir,
+            });
+            idx += 1;
+        }
 
-            successors[idx] = Some(Node {
-                path: next_path,
+        successors
+    }
+
+    fn get_super_successors(&self, map: &[Vec<u32>]) -> [Option<Crucible>; 3] {
+        let width = map[0].len();
+        let height = map.len();
+
+        let mut successors = [None, None, None];
+        let mut idx = 0;
+
+        for dir in Dir::all() {
+            if self.consecultive_dir < 4 && dir != self.dir {
+                continue;
+            }
+
+            if self.consecultive_dir >= 10 && dir == self.dir {
+                continue;
+            }
+
+            if self.dir.inverse() == dir {
+                continue;
+            }
+
+            let next_pos = (self.pos.0 + dir.x(), self.pos.1 + dir.y());
+
+            if next_pos.0 < 0
+                || next_pos.1 < 0
+                || next_pos.0 as usize >= width
+                || next_pos.1 as usize >= height
+            {
+                continue;
+            }
+
+            let next_heat_loss = self.heat_loss + map[next_pos.1 as usize][next_pos.0 as usize];
+            let next_consecultive_dir = if self.dir == dir {
+                self.consecultive_dir + 1
+            } else {
+                1
+            };
+
+            successors[idx] = Some(Crucible {
                 heat_loss: next_heat_loss,
                 pos: next_pos,
                 dir,
@@ -60,19 +104,19 @@ impl Node {
     }
 }
 
-impl std::cmp::PartialEq for Node {
+impl std::cmp::PartialEq for Crucible {
     fn eq(&self, other: &Self) -> bool {
         self.heat_loss == other.heat_loss && self.pos == other.pos
     }
 }
 
-impl std::cmp::Ord for Node {
+impl std::cmp::Ord for Crucible {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
         other.heat_loss.cmp(&self.heat_loss)
     }
 }
 
-impl std::cmp::PartialOrd for Node {
+impl std::cmp::PartialOrd for Crucible {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
         Some(self.cmp(other))
     }
@@ -117,12 +161,21 @@ impl Dir {
     }
 }
 
-fn dijkstra(map: &[Vec<u32>], start: (i32, i32), end: (i32, i32)) -> u32 {
+enum CrucibleType {
+    Normal,
+    Super,
+}
+
+fn dijkstra(
+    map: &[Vec<u32>],
+    start: (i32, i32),
+    end: (i32, i32),
+    crucible_type: CrucibleType,
+) -> u32 {
     let mut heap = BinaryHeap::new();
     let mut seen = HashSet::new();
 
-    heap.push(Node {
-        path: vec![start],
+    heap.push(Crucible {
         heat_loss: 0,
         pos: start,
         dir: Dir::Right,
@@ -130,25 +183,19 @@ fn dijkstra(map: &[Vec<u32>], start: (i32, i32), end: (i32, i32)) -> u32 {
     });
 
     while let Some(node) = heap.pop() {
-        if node.pos == end {
-            let path = node.path;
-            // println!("Path: {path:?}");
-            // map.iter().enumerate().for_each(|(y, line)| {
-            //     line.iter().enumerate().for_each(|(x, &c)| {
-            //         if path.contains(&(x as i32, y as i32)) {
-            //             print!("*");
-            //         } else {
-            //             print!("{c}");
-            //         }
-            //     });
-            //
-            //     println!();
-            // });
-            // println!();
+        if node.pos == end
+            && (matches!(crucible_type, CrucibleType::Normal) || node.consecultive_dir >= 4)
+        {
             return node.heat_loss;
         }
 
-        for successor in node.get_successors(map).into_iter().flatten() {
+        let successors = if matches!(crucible_type, CrucibleType::Normal) {
+            node.get_successors(map)
+        } else {
+            node.get_super_successors(map)
+        };
+
+        for successor in successors.into_iter().flatten() {
             if seen.contains(&(successor.consecultive_dir, successor.dir, successor.pos)) {
                 continue;
             } else {
@@ -175,11 +222,23 @@ fn part01(input: &str) -> u64 {
     let end_x = map[0].len() as i32 - 1;
     let end_y = map.len() as i32 - 1;
 
-    dijkstra(&map, (0, 0), (end_x, end_y)) as u64
+    dijkstra(&map, (0, 0), (end_x, end_y), CrucibleType::Normal) as u64
 }
 
-fn part02(_input: &str) -> u64 {
-    0
+fn part02(input: &str) -> u64 {
+    let map = input
+        .lines()
+        .map(|line| {
+            line.chars()
+                .map(|c| c.to_digit(10).unwrap())
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
+
+    let end_x = map[0].len() as i32 - 1;
+    let end_y = map.len() as i32 - 1;
+
+    dijkstra(&map, (0, 0), (end_x, end_y), CrucibleType::Super) as u64
 }
 
 fn main() {
@@ -211,7 +270,17 @@ mod test {
 
     #[test]
     fn part02() {
-        assert_eq!(super::part02(INPUT), 0);
+        assert_eq!(super::part02(INPUT), 94);
+    }
+
+    #[test]
+    fn part02_2() {
+        let input = "111111111111
+999999999991
+999999999991
+999999999991
+999999999991";
+        assert_eq!(super::part02(input), 71);
     }
 }
 
