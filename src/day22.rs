dae7bbac334f62d8597cd457d8048a5b8b6f901c
Afonso Lage <lage.afonso@gmail.com>
2024-01-07 07:18:14 -0300

    Finished day 22 part 02

--- src/day22.rs
+++ src/day22.rs
@@ -1,3 +1,5 @@
+use std::collections::VecDeque;
+
 #[derive(Default, Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 struct Vec3 {
     x: i32,
@@ -22,27 +24,22 @@ impl std::fmt::Display for Vec3 {
 }
 
 #[derive(Default, Debug, Clone, Copy)]
-struct Brick {
-    id: u32,
+struct BoundingBox {
     begin: Vec3,
     end: Vec3,
 }
 
-impl Brick {
+impl std::fmt::Display for BoundingBox {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}~{}", self.begin, self.end)
+    }
+}
+
+impl BoundingBox {
     fn move_down(mut self) -> Self {
         self.begin.z -= 1;
         self.end.z -= 1;
         self
-    }
-
-    fn intersects(&self, brick: Brick) -> bool {
-        assert_ne!(self.id, brick.id);
-        self.begin.x <= brick.end.x
-            && self.end.x >= brick.begin.x
-            && self.begin.y <= brick.end.y
-            && self.end.y >= brick.begin.y
-            && self.begin.z <= brick.end.z
-            && self.end.z >= brick.begin.z
     }
 
     fn move_up(mut self) -> Self {
@@ -50,181 +47,169 @@ impl Brick {
         self.end.z += 1;
         self
     }
+
+    fn intersects(&self, other: &Self) -> bool {
+        self.begin.x <= other.end.x
+            && self.end.x >= other.begin.x
+            && self.begin.y <= other.end.y
+            && self.end.y >= other.begin.y
+            && self.begin.z <= other.end.z
+            && self.end.z >= other.begin.z
+    }
+}
+
+#[derive(Default, Debug, Clone)]
+struct Brick {
+    id: usize,
+    bb: BoundingBox,
+    above: Vec<usize>,
+    below: Vec<usize>,
+}
+
+impl Brick {
+    fn letter(&self) -> char {
+        ((self.id as u8 % 25) + 65).try_into().unwrap()
+    }
 }
 
 impl std::fmt::Display for Brick {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "[{}] {}~{}", self.id, self.begin, self.end)
+        write!(f, "[{}][{}] {}", self.letter(), self.id, self.bb)
     }
-}
-
-fn can_move_down(bricks: &[Brick], brick: Brick) -> bool {
-    let brick = brick.move_down();
-    brick.end.z > 0
-        && !bricks
-            .iter()
-            .filter(|b| b.id != brick.id)
-            .any(|b| b.intersects(brick))
 }
 
 fn settle_down(bricks: &mut [Brick]) {
     for i in 0..bricks.len() {
-        let mut brick = bricks[i];
+        while bricks[i].bb.begin.z > 1 {
+            let below = bricks[i].bb.move_down();
 
-        if brick.end.z == 1 {
-            continue;
+            if bricks
+                .iter()
+                .filter(|b| b.id != i)
+                .any(|b| b.bb.intersects(&below))
+            {
+                break;
+            } else {
+                bricks[i].bb = below;
+            }
         }
+    }
 
-        while can_move_down(bricks, brick) {
-            brick = brick.move_down();
-        }
-        bricks[i] = brick;
+    set_above_and_below(bricks);
+}
+
+fn set_above_and_below(bricks: &mut [Brick]) {
+    for i in 0..bricks.len() {
+        let below = bricks[i].bb.move_down();
+        bricks[i].below = bricks
+            .iter()
+            .filter_map(|b| {
+                if b.id != i && below.intersects(&b.bb) {
+                    Some(b.id)
+                } else {
+                    None
+                }
+            })
+            .collect();
+
+        let above = bricks[i].bb.move_up();
+        bricks[i].above = bricks
+            .iter()
+            .filter_map(|b| {
+                if b.id != i && above.intersects(&b.bb) {
+                    Some(b.id)
+                } else {
+                    None
+                }
+            })
+            .collect();
     }
 }
 
-fn get_upper_bricks(bricks: &[Brick], brick: Brick) -> Vec<Brick> {
-    let brick = brick.move_up();
+fn can_remove(bricks: &[Brick], brick: &Brick) -> bool {
     bricks
         .iter()
-        .filter(|b| b.id != brick.id)
-        .filter(|b| b.intersects(brick))
-        .copied()
-        .collect()
+        .filter(|b| brick.above.contains(&b.id))
+        .all(|b| b.below.len() > 1)
 }
 
-fn can_remove(bricks: &[Brick], brick: Brick) -> bool {
-    let upper_bricks = get_upper_bricks(bricks, brick);
-    upper_bricks.into_iter().all(|b| {
-        let count = count_intersections(bricks, b.move_down());
-        count > 1
-    })
-}
+fn chain_reaction(bricks: &[Brick]) -> usize {
+    let mut queue = VecDeque::new();
+    let mut falling = vec![false; bricks.len()];
+    let mut count = 0;
 
-fn count_intersections(bricks: &[Brick], brick: Brick) -> usize {
-    let intersections = bricks
-        .iter()
-        .filter(|b| b.id != brick.id)
-        .filter(|b| b.intersects(brick))
-        .collect::<Vec<_>>();
+    for b in bricks {
+        queue.push_back(b.id);
+        falling[b.id] = true;
 
-    intersections.len()
-}
-
-fn print_y_z(bricks: &[Brick]) {
-    let (width, height) = bricks.iter().fold((0, 0), |(width, height), brick| {
-        let width = width.max(brick.end.y);
-        let height = height.max(brick.end.z);
-
-        (width, height)
-    });
-    println!("Z");
-    for _ in 0..width + 3 {
-        print!("_");
-    }
-    println!();
-    for z in (1..height + 1).rev() {
-        print!("|");
-        for y in 0..(width + 1) {
-            let p = Vec3 { x: 1, y, z };
-            if let Some(brick) = bricks
-                .iter()
-                .find(|b| b.begin.y <= p.y && b.end.y >= p.y && b.begin.z <= p.z && b.end.z >= p.z)
-            {
-                print!("{}", brick.id);
-            } else {
-                print!(" ");
+        while let Some(id) = queue.pop_front() {
+            for &upper_id in &bricks[id].above {
+                if !falling[upper_id] && will_fall(&bricks[upper_id], &falling) {
+                    queue.push_back(upper_id);
+                    falling[upper_id] = true;
+                    count += 1;
+                }
             }
         }
-        print!("|");
-        println!();
+        falling.fill(false);
     }
 
-    for _ in 0..width + 3 {
-        print!("¯");
-    }
-    println!(" Y");
+    count
 }
 
-fn print_x_z(bricks: &[Brick]) {
-    let (width, height) = bricks.iter().fold((0, 0), |(width, height), brick| {
-        let width = width.max(brick.end.x);
-        let height = height.max(brick.end.z);
-
-        (width, height)
-    });
-    println!("Z");
-    for _ in 0..width + 3 {
-        print!("_");
-    }
-    println!();
-    for z in (1..height + 1).rev() {
-        print!("|");
-        for x in 0..(width + 1) {
-            let p = Vec3 { x, y: 1, z };
-            if let Some(brick) = bricks
-                .iter()
-                .find(|b| b.begin.x <= p.x && b.end.x >= p.x && b.begin.z <= p.z && b.end.z >= p.z)
-            {
-                print!("{}", brick.id);
-            } else {
-                print!(" ");
-            }
-        }
-        print!("|");
-        println!();
-    }
-    for _ in 0..width + 3 {
-        print!("¯");
-    }
-    println!(" X");
-}
-
-fn print_bricks(bricks: &[Brick]) {
-    print_x_z(bricks);
-    print_y_z(bricks);
+fn will_fall(brick: &Brick, falling: &[bool]) -> bool {
+    brick.below.iter().all(|&id| falling[id])
 }
 
 fn parse_bricks(input: &str) -> Vec<Brick> {
-    let mut next_id = 0;
     let mut bricks = input
         .lines()
         .map(|line| {
             line.split_once('~')
                 .map(|(begin, end)| {
-                    let begin =
+                    let mut begin =
                         Vec3::from_iter(begin.split(',').map(|n| n.parse::<i32>().unwrap()));
-                    let end = Vec3::from_iter(end.split(',').map(|n| n.parse::<i32>().unwrap()));
-                    let id = next_id;
-                    next_id += 1;
-                    Brick { begin, end, id }
+                    let mut end =
+                        Vec3::from_iter(end.split(',').map(|n| n.parse::<i32>().unwrap()));
+
+                    if begin.x > end.x {
+                        std::mem::swap(&mut begin.x, &mut end.x);
+                    }
+                    if begin.y > end.y {
+                        std::mem::swap(&mut begin.y, &mut end.y);
+                    }
+                    if begin.z > end.z {
+                        std::mem::swap(&mut begin.z, &mut end.z);
+                    }
+
+                    let bb = BoundingBox { begin, end };
+                    Brick {
+                        bb,
+                        ..Default::default()
+                    }
                 })
                 .unwrap()
         })
         .collect::<Vec<_>>();
 
-    bricks.sort_unstable_by(|a, b| a.begin.z.cmp(&b.begin.z));
+    bricks.sort_unstable_by_key(|b| b.bb.end.z);
+    bricks.iter_mut().enumerate().for_each(|(id, b)| b.id = id);
 
     bricks
 }
 
 fn part01(input: &str) -> u64 {
     let mut bricks = parse_bricks(input);
-
-    print_bricks(&bricks);
-
     settle_down(&mut bricks);
 
-    print_bricks(&bricks);
-
-    bricks
-        .iter()
-        .copied()
-        .filter(|&b| can_remove(&bricks, b))
-        .count() as u64
+    bricks.iter().filter(|&b| can_remove(&bricks, b)).count() as u64
 }
 
-fn part02(_input: &str) -> u64 {
-    0
+fn part02(input: &str) -> u64 {
+    let mut bricks = parse_bricks(input);
+    settle_down(&mut bricks);
+
+    chain_reaction(&bricks) as u64
 }
 
 fn main() {
